1bc31ba8c41d386bd04b5fdf4a1a8a72f04388aa
ssoranaokil <naoki6117@gmail.com>
2023-05-25 20:57:11 +0900

    httpsの設定home

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = secure_url('/');
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
